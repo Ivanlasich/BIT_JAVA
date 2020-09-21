856bdb613e9620720063acba1b28d4083f7b1ae1
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-09-21 13:32:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package First_task;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello word");
+        Customer example;
+        example = new Customer("  ", "joj");
+        System.out.println(example.fullName());
+    }
+}
