ff95ee1dff24a975229f7db74afc66e02bb95957
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-10-05 18:52:36 +0300

    Add files via upload

--- TestAccount.java
+++ TestAccount.java
@@ -1,49 +1,51 @@
 package First_task;
 import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
+
 public class TestAccount {
     @Test
     public void withdrawNoActiveAccount() {
         Customer person = new Customer("John", "Goodman");
-        assert person.withdrawFromCurrentAccount(10) == false;
+        assertTrue(person.withdrawFromCurrentAccount(10) == false);
     }
 
     @Test
     public void addMoneyNoActiveAccount() {
         Customer person = new Customer("John", "Goodman");
-        assert person.addMoneyToCurrentAccount(10) == false;
+        assertTrue(person.addMoneyToCurrentAccount(10) == false);
     }
 
     @Test
     public void accountWithdrawNegativeAmount() {
         Account acc = new Account(0);
         acc.add(11.23);
-        assert acc.withdraw(-23) == false;
+        assertTrue(acc.withdraw(-23) == false);
     }
 
     @Test
     public void accountWithdrawZeroAmount() {
         Account acc = new Account(0);
         acc.add(11.23);
-        assert acc.withdraw(0) == false;
+        assertTrue(acc.withdraw(0) == false);
     }
 
     @Test
     public void accountWithdrawNegativeDiff() {
         Account acc = new Account(0);
         acc.add(11.23);
-        assert acc.withdraw(23) == false;
+        assertTrue(acc.withdraw(23) == false);
     }
 
     @Test
     public void accountAddNegativeAmount() {
         Account acc = new Account(0);
-        assert acc.add(-11.23) == false;
+        assertTrue( acc.add(-11.23) == false);
     }
 
     @Test
     public void accountAddZeroAmount() {
         Account acc = new Account(0);
-        assert acc.add(0) == false;
+        assertTrue( acc.add(0) == false);
     }
 
 }
