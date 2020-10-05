d8a7bac33efe734b20fe37dd993372dfa1f81d62
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-10-05 18:53:13 +0300

    Add files via upload

--- TestCustomer.java
+++ TestCustomer.java
@@ -2,24 +2,26 @@ package First_task;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class TestCustomer {
     @Test
     public void customerAlreadyHasAccount() {
         Customer person = new Customer("John", "Goodman");
         person.openAccount(0);
-        assert person.openAccount(0) == false;
+        assertTrue( person.openAccount(0) == false);
     }
 
     @Test
     public void customerNoActiveAccount() {
         Customer person = new Customer("John", "Goodman");
-        assert person.closeAccount() == false;
+        assertTrue( person.closeAccount() == false);
     }
 
     @Test
     public void customerFullName() {
         Customer person = new Customer("John", "Goodman");
-        assert person.fullName().equals("John Goodman");
+        assertTrue( person.fullName().equals("John Goodman"));
     }
 
 }
