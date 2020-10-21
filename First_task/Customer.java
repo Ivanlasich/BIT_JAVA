51567c858f384a759191f54d12f889d3f48e37d0
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-10-21 14:27:53 +0300

    Add files via upload

--- First_task/Customer.java
+++ First_task/Customer.java
@@ -11,22 +11,19 @@ public class Customer {
     }
 
     /**
-     * Opens account for a customer (creates Account and sets it to field "account").
+     * Opens account for a customer (creates DebitCard and sets it to field "account").
      * Customer can't have greater than one opened account.
      *
      * @param accountId id of the account
      * @return true if account hasn't already created, otherwise returns false and prints "Customer fullName() already has the active account"
      */
     public boolean openAccount(long accountId) {
-        // write your code here
-        if (account != null){
+        if (account != null) {
             System.out.println("Customer " + this.fullName() + " already has the active account");
             return false;
         }
-        else {
-            account = new Account(accountId);
-            return true;
-        }
+        account = new Account(accountId);
+        return true;
     }
 
     /**
@@ -35,54 +32,49 @@ public class Customer {
      * @return false if account is already null and prints "Customer fullName() has no active account to close", otherwise sets account to null and returns true
      */
     public boolean closeAccount() {
-        if (account == null){
+        if (account == null) {
             System.out.println("Customer " + this.fullName() + " has no active account to close");
             return false;
         }
-        else {
-            account = null;
-            return true;
-        }
+
+        account = null;
+        return true;
     }
 
     /**
      * Formatted full name of the customer
+     *
      * @return concatenated form of name and lastName, e.g. "John Goodman"
      */
     public String fullName() {
-        // write your code here
         return name + " " + lastName;
     }
 
     /**
-     * Delegates withdraw to Account class
+     * Delegates withdraw to DebitCard class
+     *
      * @param amount
-     * @return false if account is null and prints "Customer fullName() has no active account", otherwise returns the result of Account's withdraw method
+     * @return false if account is null and prints "Customer fullName() has no active account", otherwise returns the result of DebitCard's withdraw method
      */
     public boolean withdrawFromCurrentAccount(double amount) {
-        // write your code here
-        if (account == null){
+        if (account == null) {
             System.out.println("Customer " + this.fullName() + " has no active account");
             return false;
         }
-        else {
-            return account.withdraw(amount);
-        }
+        return account.withdraw(amount);
     }
 
     /**
-     * Delegates adding money to Account class
+     * Delegates adding money to DebitCard class
+     *
      * @param amount
-     * @return false if account is null and prints "Customer fullName() has no active account", otherwise returns the result of Account's add method
+     * @return false if account is null and prints "Customer fullName() has no active account", otherwise returns the result of DebitCard's add method
      */
     public boolean addMoneyToCurrentAccount(double amount) {
-        // write your code here
-        if (account == null){
+        if (account == null) {
             System.out.println("Customer " + this.fullName() + " has no active account");
             return false;
         }
-        else {
-            return account.add(amount);
-        }
+        return account.add(amount);
     }
 }
