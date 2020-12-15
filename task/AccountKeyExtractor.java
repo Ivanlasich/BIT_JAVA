d4106bd1eebcadad63a36caa43c261a7f8e77ef9
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-12-15 21:23:22 +0300

    Delete AccountKeyExtractor.java

--- task/AccountKeyExtractor.java
+++
@@ -1,9 +0,0 @@
-package task;
-
-public class AccountKeyExtractor implements KeyExtractor {
-
-    @Override
-    public Object extract(Object entity) {
-        return ((DebitCard) entity).getId();
-    }
-}
