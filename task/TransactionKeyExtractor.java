b00bba98b6cf88294d143cc77a5ef40e68bd14b7
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-12-15 21:25:31 +0300

    Delete TransactionKeyExtractor.java

--- task/TransactionKeyExtractor.java
+++
@@ -1,9 +0,0 @@
-package task;
-
-
-public class TransactionKeyExtractor implements KeyExtractor {
-    @Override
-    public Object extract(Object entity) {
-        return ((Transaction) entity).getId();
-    }
-}
