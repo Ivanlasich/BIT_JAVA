b09c2c436bb7bb96ee5fa79c1df6bcf04bad0a73
Ivanlasich <58664580+Ivanlasich@users.noreply.github.com>
2020-12-15 21:35:10 +0300

    Delete EntryKeyExtractor.java

--- task/EntryKeyExtractor.java
+++
@@ -1,8 +0,0 @@
-package task;
-
-public class EntryKeyExtractor implements KeyExtractor {
-    @Override
-    public Object extract(Object entity) {
-        return ((Entry) entity).getTime();
-    }
-}
